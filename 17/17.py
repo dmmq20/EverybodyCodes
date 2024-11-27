51539cc292eafd58cc2d439a9acb06fb7f2fbd06
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-11-27 19:30:32 +0000

    17 parts 1-3

---
+++ 17/17.py
@@ -0,0 +1,46 @@
+import heapq, math
+
+def parse(f):
+    stars = set()
+    for r, row in enumerate(open(f).read().splitlines()):
+        for c, ch in enumerate(row):
+            if ch == "*":
+                stars.add((r, c))
+    return stars
+
+def prims(stars, max_dist=None):
+    pq = [(0, stars.pop())]
+    seen = set()
+    ans = 0
+    while pq:
+        dist, (r, c) = heapq.heappop(pq)
+        if (r, c) in seen:
+            continue
+        seen.add((r,c ))
+        ans += dist
+        for star in stars:
+            if star in seen:
+                continue
+            dist = abs(star[0] - r) + abs(star[1] - c)
+            if not max_dist or dist < max_dist:
+                heapq.heappush(pq, (dist, star))
+    stars -= seen
+    return ans + len(seen)
+
+def solve1():
+    stars = parse("1.txt")
+    return prims(stars)
+
+def solve2():
+    stars = parse("2.txt")
+    return prims(stars)
+
+def solve3():
+    constellations = []
+    stars = parse("3.txt")
+    while stars:
+        constellations.append(prims(stars, 6))
+    constellations.sort()
+    return math.prod(constellations[-3:])
+
+print(solve1(), solve2(), solve3())
